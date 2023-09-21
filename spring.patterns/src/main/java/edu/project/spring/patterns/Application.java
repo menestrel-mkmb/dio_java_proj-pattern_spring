6cb9cb05da8c559a6e35346ef9c8af8ca7b963b4
Michael Kevin <michaelkmb@hotmail.com>
2023-09-21 14:48:55 -0300

    Project HelloWorld running;

---
+++ spring.patterns/src/main/java/edu/project/spring/patterns/Application.java
@@ -0,0 +1,19 @@
+package edu.project.spring.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/*
+*
+* Projeto para demonstrar padrões de projeto
+* a partir do Spring Boot Framework
+*
+* */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
