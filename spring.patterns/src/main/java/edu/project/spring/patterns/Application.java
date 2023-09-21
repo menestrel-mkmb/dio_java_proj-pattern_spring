d0eb3a8c48b8402457f63a87d5fa26e19ed7b997
Michael Kevin <michaelkmb@hotmail.com>
2023-09-21 14:55:35 -0300

    + H2 DB settings n Swagger via OpenAPI;

--- spring.patterns/src/main/java/edu/project/spring/patterns/Application.java
+++ spring.patterns/src/main/java/edu/project/spring/patterns/Application.java
@@ -2,6 +2,7 @@ package edu.project.spring.patterns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /*
 *
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 * a partir do Spring Boot Framework
 *
 * */
+@EnableFeignClients
 @SpringBootApplication
 public class Application {
 
